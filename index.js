556e1ae63f6b0e51a02ecfcf4c2632e5eff028c8
SAGAR BHAGAT <SAGARBHAGAT43@GMAIL.COM>
2022-08-25 16:53:12 +0530

    commint initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+let score1 = document.getElementById("sc-1")
+let score2 = document.getElementById("sc-2")
+let c1score=0;
+
+function sc1change1 (){
+    c1score+=1;
+    score1.textContent=c1score;
+
+}
+
+function sc1change2 (){
+    c1score+=2;
+    score1.textContent=c1score;
+
+}
+function sc1change3 (){
+    c1score+=3;
+    score1.textContent=c1score;
+
+}
